3818fc5249ba5620e27bfbde6b55b7c341be3e50
riku-noname <rmorimoto@mikilab.doshisha.ac.jp>
2018-09-23 01:20:34 +0900

    左上に文字列の出力機能の実装

--- konikaminolta_hackathon/faceCamera.py
+++ konikaminolta_hackathon/faceCamera.py
@@ -39,7 +39,10 @@ if __name__ == '__main__':
         img = c_frame
         #img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         face_list = cascade.detectMultiScale(img, scaleFactor=1.11, minNeighbors=3, minSize=(100, 100))
-
+        #文字の出力（会議名，ユーザ名）
+        cv2.putText(img,'MeetingName', (5,40), cv2.FONT_HERSHEY_COMPLEX, 1.5,(255, 255, 255))
+        cv2.line(img,(2,55),(345,55),(255,255,255),2)
+        cv2.putText(img,'UserName', (15,85), cv2.FONT_HERSHEY_SIMPLEX, 1.0,(255, 255, 255))
         # 検出した顔に印を付ける
         for (x, y, w, h) in face_list:
             color = (0, 0, 225)
@@ -53,26 +56,27 @@ if __name__ == '__main__':
             for (mx, my, mw, mh) in mouse:
                 cv2.rectangle(mouse_color,(mx,my),(mx+mw,my+mh),(0,255,0),2)
                 # 口だけ切り出して二値化画像を保存
-                threshold = 40
+                threshold = 40 #二値化の閾値の設定
                 for num, rect in enumerate(mouse):
                     x = rect[0]
                     y = rect[1]
                     width = rect[2]
                     height = rect[3]
                     dst = mouse_gray[y:y + height, x:x + width]
+                    #二値化処理
                     ret,img_threshold = cv2.threshold(dst,threshold,255,cv2.THRESH_BINARY)
-                    #cv2.imshow("img_threshold",img_threshold)
                     cv2.imwrite("./output/img_threshold.jpg",img_threshold)
-                    #print('savedimages.')
                     binary_img = cv2.imread("./output/img_threshold.jpg")
-                    cnt =0
+                    cnt =0 #黒色領域の数を格納する変数
                     for val in binary_img.flat:
                         if val == 0:
                             cnt += 1
                     cv2.waitKey(1)
+                    #二値化画像の黒色領域が何箇所あるかの判断→口が開いていれば黒いろ領域が多くなる＝発言している
                     if cnt > 600:
                         #print(speaking_counter, "Speaking!!")
                         speaking_counter += 1
+                    #発言カウンターが10個貯まれば，サーバに秒数の送信
                     if speaking_counter == 10:
                         print("You spoke ", speaking_counter * 0.21, "second")
                         response = requests.post('https://jsondata.okiba.me/v1/json/m1NvW180922122602', data={'start': speaking_counter * 0.21})
@@ -80,7 +84,6 @@ if __name__ == '__main__':
                         speaking_counter = 0
                     else:
                         continue
-                    #print("done.")
 
         # フレーム表示
         cv2.imshow(ORG_WINDOW_NAME, c_frame)
